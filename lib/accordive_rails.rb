f612419f0d6d6d39a32c4d80ae07e9ab9caf6456
Jon Calhoun <joncalhoun@gmail.com>
2014-03-06 00:08:24 -0800

    updating style a bit and adding breadcrumbs

--- lib/accordive_rails.rb
+++ lib/accordive_rails.rb
@@ -4,8 +4,10 @@ require "accordive_rails/node"
 require "accordive_rails/association"
 require "accordive_rails/singleton"
 require "accordive_rails/web"
+require 'accordive_rails/railtie'
 
 # Dir[File.dirname(__FILE__) + "/accordive_rails/*"].each {|file| require file }
 
 module AccordiveRails
 end
+
