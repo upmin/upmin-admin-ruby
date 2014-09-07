a08e5c5fa53b16713043b2446021481dedf7192a
Jon Calhoun <joncalhoun@gmail.com>
2014-09-07 18:08:15 -0400

    updating a few files to be cleaner since they had unused code

--- lib/upmin/railties/render.rb
+++ lib/upmin/railties/render.rb
@@ -62,6 +62,7 @@ module Upmin::Railties
     end
 
 
+    # Generic render method that is used by all of the up_<something> methods. Tries to render the partials in order, passing data in as the :object, along with options.
     def up_render(data, partials, options = {})
       # Use options as the render hash, and set :object as the data being used for rendering.
       options[:object] = data
@@ -78,64 +79,5 @@ module Upmin::Railties
       raise "Failed to find a matching partial while trying to render the following: #{object.inspect}"
     end
 
-
-
-
-
-
-
-
-
-    def upmin_render(model_or_node, options = {})
-      if model_or_node.is_a?(::ActiveRecord::Base)
-        upmin_render_node(Upmin::Graph::ModelNode.new(model_or_node), options)
-
-      else # is a node
-        upmin_render_node(model_or_node, options)
-      end
-    end
-
-    def upmin_render_search_box(upmin_model, options = {})
-      partials = RenderHelpers.partials_for_search_box(upmin_model, options)
-
-      partials.each do |partial|
-        begin
-          return render(
-            partial: partial,
-            object: upmin_model, # this doesn't work with nil
-            locals: {
-              upmin_model: upmin_model,
-              options: options
-            }
-          )
-        rescue ActionView::MissingTemplate => e
-        end
-      end
-      raise "Failed to find a matching partial, even with fallback :unknown partials"
-    end
-
-
-
-    def upmin_render_node(node, options = {})
-      partials = RenderHelpers.partials_for(node, options)
-
-      # TODO(jon): Update this to use something like https://coderwall.com/p/ftbmsa instead of a rescue
-      partials.each do |partial|
-        puts "Trying #{partial}"
-        begin
-          return render(
-            partial: partial,
-            object: node.object, # this doesn't work with nil
-            locals: {
-              node: node,
-              options: options,
-              RenderHelpers.object_name(partial) => node.object
-            }
-          )
-        rescue ActionView::MissingTemplate => e
-        end
-      end
-      raise "Failed to find a matching partial, even with fallback :unknown partials"
-    end
   end
 end
