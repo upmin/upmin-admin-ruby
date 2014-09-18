025d0e42befc22966c09244d00246ab8dc928c0d
Jon Calhoun <joncalhoun@gmail.com>
2014-09-17 23:31:35 -0400

    updating spec to use expect instead of .should

--- spec/requests/search_spec.rb
+++ spec/requests/search_spec.rb
@@ -10,34 +10,30 @@ feature('Search') do
   scenario("pagination") do
     visit('/upmin/m/User')
 
-    puts User.all.as_json
-    puts "html=#{page.html}"
-    page.save_and_open_page
-
     # Make sure some basic pagination exits
     within('.pagination') do
       within('.current') do
-        page.should(have_content('1'))
+        expect(page).to(have_content('1'))
       end
 
-      page.should(have_content('Next'))
+      expect(page).to(have_content('Next'))
       click_link('Next')
     end
 
     within('.pagination') do
       within('.current') do
-        page.should(have_content('2'))
+        expect(page).to(have_content('2'))
       end
 
-      page.should(have_content('Next'))
-      page.should(have_content('Prev'))
+      expect(page).to(have_content('Next'))
+      expect(page).to(have_content('Prev'))
 
       click_link('3')
     end
 
     within('.pagination') do
       within('.current') do
-        page.should(have_content('3'))
+        expect(page).to(have_content('3'))
       end
     end
 
