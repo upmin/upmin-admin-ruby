b62bbe0c195b759dfa37a8d77f4e9b5069dd7104
mbrookes <you@example.com>
2014-10-02 11:15:55 +0100

    add bootstrap styling for will_paginate -  `will_paginate-bootstrap` being added to parent Rails app Gemfile

--- lib/upmin/railties/paginator.rb
+++ lib/upmin/railties/paginator.rb
@@ -4,6 +4,7 @@ module Upmin::Railties
 
     def up_paginate(scope, options = {}, &block)
       if defined?(WillPaginate)
+        options[:renderer] = BootstrapPagination::Rails if defined?(BootstrapPagination::Rails)
         return will_paginate(scope, options)
       else # Use Kaminari
         return paginate(scope, options, &block)
