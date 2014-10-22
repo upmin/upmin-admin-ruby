e7810d7c7426c24ee734def6ec9400aac40a1701
mbrookes <you@example.com>
2014-10-22 23:48:20 +0100

    add page_entries_info to model/search view
    Supports both kaminari and will_paginate, with ActiveModel and DataMapper

--- spec/features/search_spec.rb
+++ spec/features/search_spec.rb
@@ -13,18 +13,13 @@ feature("Search Views") do
   scenario("Page Entries Info") do
     visit("/upmin/m/User")
 
-    # Make sure some basic page entries info exits
-    within(".page_entries_info:first-of-type") do
+    # Make sure some basic page entries info exists
+    within(".page-entries-info") do
       within("b:first-of-type") do
         expect(page).to(have_content("1 - 30"))
       end
 
       expect(page).to(have_content("of"))
-
-      within("b:nth-of-type(2)") do
-        expect(page).to(have_content("100"))
-      end
-
       expect(page).to(have_content("in total"))
     end
   end
@@ -32,30 +27,36 @@ feature("Search Views") do
   scenario("Pagination") do
     visit("/upmin/m/User")
 
-    # Make sure some basic pagination exits
-    within(".pagination:first-of-type") do
-      within(".active, .current") do
-        expect(page).to(have_content("1"))
-      end
+    # Make sure some basic pagination exists
+    within(".col-sm-6:nth-of-type(2)") do
+      within(".pagination") do
+        within(".active, .current") do
+          expect(page).to(have_content("1"))
+        end
 
-      expect(page).to(have_content("Next"))
-      click_link("Next")
+        expect(page).to(have_content("Next"))
+        click_link("Next")
+      end
     end
 
-    within(".pagination:nth-of-type(2)") do
-      within(".active, .current") do
-        expect(page).to(have_content("2"))
+    within(".col-sm-6:nth-of-type(2)") do
+      within(".pagination") do
+        within(".active, .current") do
+          expect(page).to(have_content("2"))
+        end
+
+        expect(page).to(have_content("Next"))
+        expect(page).to(have_content("Prev"))
+
+        click_link("3")
       end
-
-      expect(page).to(have_content("Next"))
-      expect(page).to(have_content("Prev"))
-
-      click_link("3")
     end
 
-    within(".pagination:last-of-type") do
-      within(".active, .current") do
-        expect(page).to(have_content("3"))
+    within(".col-md-12") do
+      within(".pagination") do
+        within(".active, .current") do
+          expect(page).to(have_content("3"))
+        end
       end
     end
   end
@@ -70,18 +71,6 @@ feature("Search Views") do
     click_button("Search")
 
     expect(page).to(have_selector("a.search-result-link", count: 5))
-  end
-
-  scenario("Search via string") do
-    expected_user = User.first
-
-    visit("/upmin/m/User")
-
-    fill_in("q_name_cont", with: expected_user.name)
-    click_button("Search")
-
-    expect(page).to(have_selector("a.search-result-link", minimum: 1))
-    expect(page).to(have_content(expected_user.name))
   end
 
   scenario("Search via string") do
