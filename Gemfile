99fc7a03046796bcf51b01b4b10938885b1d8465
mbrookes <git@nospam.33m.co>
2014-11-15 22:14:14 +0000

    Add grouping helper methods - eliminate groupdate / dateslices dependancy

--- Gemfile
+++ Gemfile
@@ -2,6 +2,6 @@ source "https://rubygems.org"
 
 gemspec
 # TODO (matt): Move to gemspec
-gem 'dateslices', path: '../../gems/dateslices', branch: 'output_format'
+#gem 'dateslices', path: '../../gems/dateslices', branch: 'output_format'
 #gem 'by_star'
-#gem 'groupdate'
+#gem 'groupdate', github:'mieko/groupdate', branch: 'sqlite3'
--- app/helpers/upmin/dashboard_helper.rb
+++ app/helpers/upmin/dashboard_helper.rb
@@ -1,22 +1,22 @@
 module Upmin
   module DashboardHelper
 
-    # call the appropriate count_by_ method according to the time range of model objects
-    def group_by_time(model)
-      return model.send("count_by_#{group_by model}")
+    # call the appropriate by_??? method according to the time range of the model
+    def group_by_best_fit(model)
+      @model = model
+      return send "by_#{group_by model}"
     end
 
     def group_by(model)
-      range = time_range(model)
-      seconds = time_range_to_seconds(range)
+      seconds = date_range_to_seconds(date_range(model))
 
-      if seconds < 30          # 30 seconds
-        return 'second'
-      elsif seconds < 1800     # 30 minutes
-        return 'minute'
-      elsif seconds < 86400    # 24 hours
-        return 'hour'
-      elsif seconds < 2592000  # 30 days
+      #if seconds < 30          # 30 seconds
+      #  return 'second'
+      #elsif seconds < 1800     # 30 minutes
+      #  return 'minute'
+      #elsif seconds < 86400    # 24 hours
+      #  return 'hour'
+      if seconds < 2592000  # 30 days
         return 'day'
       elsif seconds < 15724800 # 26 weeks (~6 months)
         return 'week'
@@ -27,20 +27,70 @@ module Upmin
       end
     end
 
-    def time_range_to_seconds(range)
+    def date_range_to_seconds(range)
       return range.last - range.first
     end
 
-    def time_range(model)
-      return first_time_of(model)..last_time_of(model)
+    def date_range(model)
+      return first_date_of(model)..last_date_of(model)
     end
 
-    def first_time_of(model)
+    def first_date_of(model)
       return model.order("created_at ASC").first.try(:created_at) || Time.now
     end
 
-    def last_time_of(model)
+    def last_date_of(model)
       return model.order("created_at ASC").last.try(:created_at) || Time.now
+    end
+
+
+    def by_day
+      @model.group("date(created_at)").count
+    end
+
+    def by_week
+      result = Hash.new(0)
+
+      # map each "YYYY-MM-DD" key to beginning of week, then sum for same date.
+      by_day.each_with_object(result) { |i, a|  a[Date.parse(i[0].to_s).beginning_of_week.strftime] += i[1] }
+      return result
+    end
+
+    def by_month
+      return group_by_filter('^\\d{4}-\\d{2}') # YYYY-MM
+    end
+
+    def by_year
+      return group_by_filter('^\\d{4}') # YYYY
+    end
+
+    def group_by_filter(date_filter)
+      result = Hash.new(0)
+
+      # dates.map! {|date, count| [date.slice(/#{date_filter}/), count]}; dates.each { |date, count| by_date[date] += count }
+      by_day.each_with_object(result) { |i, a|  a[i[0].to_s.slice(/#{date_filter}/)] += i[1] }
+      return result
+    end
+
+    def by_day_of_week
+      template = Hash[Date::ABBR_DAYNAMES.map {|x| [x, 0]}]
+      return group_by_strftime(template, '%a')
+    end
+
+    def by_week_of_year
+      template = Hash(0)
+      return group_by_strftime(template, '%W')
+    end
+
+    def by_month_of_year
+      template = Hash[Date::ABBR_MONTHNAMES.map {|x| [x, 0]}]
+      template.shift
+      return group_by_strftime(template, '%b')
+    end
+
+    def group_by_strftime(result, filter)
+      by_day.each_with_object(result) { |i, a|  a[Date.parse(i[0].to_s).strftime(filter)] += i[1] }
+      return result
     end
 
   end
