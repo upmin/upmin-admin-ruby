e4e31251723fbbfbfa012a4cc48e5fbdf66794a9
Jason Yeo <jasonyeo88@gmail.com>
2016-03-31 15:39:30 +0800

    Protect upmin controllers from CSRF attacks

--- app/controllers/upmin/application_controller.rb
+++ app/controllers/upmin/application_controller.rb
@@ -1,4 +1,9 @@
 module Upmin
   class ApplicationController < ActionController::Base
+    if ::Rails.version.to_i < 4
+      protect_from_forgery
+    else
+      protect_from_forgery with: :exception
+    end
   end
 end
